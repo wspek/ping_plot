ec7c36dee6a2751aa89ef0ef68b3461579af1a38
Waldo Spek <waldospek@gmail.com>
2023-05-01 19:18:56 -0300

    Add logging

--- speedtest_cli_influxdb.py
+++ speedtest_cli_influxdb.py
@@ -5,15 +5,24 @@ import time
 
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
+import logging
 
 INFLUX_DB_TOKEN = 'W9ulA5wf4nIfTay7A9rhlxSb9lE4H6e1wgQgPqMW2QR0wI-gurv6il2Quu6UMN4BgDa-i6xEmgaiJaWHWDedvQ=='
 ORG = "Spek"
 BUCKET = "speedtest"
+LOG_FILE = '/var/log/speedtest-cli.log'
+
+logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 def run_speedtest():
-    output = subprocess.check_output("speedtest-cli --csv", shell=True).decode()
-    return output.strip().split(',')
+    logging.info('Running speedtest')
+
+    with open(LOG_FILE, 'a') as log_file:
+        proc = subprocess.Popen("speedtest-cli --csv", stdout=log_file, stderr=log_file, shell=True)
+        proc.communicate()
+
+    logging.info('Running speedtest done')
 
 
 def write_to_csv(data, filename='speedtest_results.csv'):
@@ -41,6 +50,8 @@ def get_influxdb_container_ip():
 
 
 def write_to_influxdb(data):
+    logging.info('Writing to influxdb')
+
     url = f"http://{get_influxdb_container_ip()}:8086"
 
     client = InfluxDBClient(url=url, token=INFLUX_DB_TOKEN)
@@ -60,11 +71,17 @@ def write_to_influxdb(data):
     write_api.write(bucket=BUCKET, org=ORG, record=point)
     client.close()
 
+    logging.info('Writing to influxdb done')
+
 
 def run():
+    logging.info('*** START ***')
+
     result = run_speedtest()
     # result = ['41338', 'Cooperativa de Colonia Caroya y Jesus Maria Ltda', 'Colonia Caroya', '2023-04-15T18:41:54.708922Z', '663.0926436032137', '78.865', '19914755.186711118', '2171532.6305631828', '', '181.102.18.27']
     write_to_influxdb(result)
+
+    logging.info('*** END ***')
 
 
 if __name__ == '__main__':
